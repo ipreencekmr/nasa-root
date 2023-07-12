04fe493d675af83723e04b0b25a3368a7e967191
Prince Kumar Sharma <ipreencekmr@gmail.com>
2023-07-12 12:56:11 +0530

    state config update

--- src/config/provideStateConfig.js
+++ src/config/provideStateConfig.js
@@ -1,9 +1,12 @@
 const NASA_DOMAIN = 'nasaDomain';
+const FAKE_DOMAIN = 'fakeDomain';
 const APP_DOMAIN = 'appDomain';
 const APOD_URI = 'apodUri';
 const NEO_URI = 'neoUri';
 const MRO_URI = 'mroUri';
 const DONKI_URI = 'donkiUri';
+const SIGNIN_URI = 'signinUri';
+const SIGNUP_URI = 'signupUri';
 const API_KEY = 'apiKey';
 
 const stateConfig = {
@@ -17,6 +20,18 @@ const stateConfig = {
       development: 'https://api.nasa.gov',
       qa: 'https://api.nasa.gov',
       production: 'https://api.nasa.gov',
+    },
+  },
+  [FAKE_DOMAIN]: {
+    client: {
+      development: 'https://fakestoreapi.com',
+      qa: 'https://fakestoreapi.com',
+      production: 'https://fakestoreapi.com',
+    },
+    server: {
+      development: 'https://fakestoreapi.com',
+      qa: 'https://fakestoreapi.com',
+      production: 'https://fakestoreapi.com',
     },
   },
   [APP_DOMAIN]: {
@@ -46,6 +61,14 @@ const stateConfig = {
   [DONKI_URI]: {
     client: '/DONKI/notifications',
     server: '/DONKI/notifications',
+  },
+  [SIGNIN_URI]: {
+    client: '/auth/login',
+    server: '/auth/login',
+  },
+  [SIGNUP_URI]: {
+    client: '/users',
+    server: '/users',
   },
   [API_KEY]: {
     client: 'Vv0TXGbYxieYxAIb0NfziTHJssra0FHhF8cn6DD9',
